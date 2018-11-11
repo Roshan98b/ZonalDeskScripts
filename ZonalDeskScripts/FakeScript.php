dc26ff1a580600f5a6eccfbd171ce95099d3b27c
Roshan Badrinath <roshan98b@gmail.com>
2018-11-11 17:53:58 +0530

    Cloud Test3

--- ZonalDeskScripts/FakeScript.php
+++ ZonalDeskScripts/FakeScript.php
@@ -7,12 +7,12 @@ $serverName = "sl-eu-gb-p00.dblayer.com:15556";
 
 $connect = mysqli_connect($serverName,$mysqlUsername,$mysqlPassword,$dbName);
 
-if($connect) {
-	echo "Connection successful!";
-}
-else {
-	echo "Connection unsuccessful!";
-}
+// if($connect) {
+// 	echo "Connection successful!";
+// }
+// else {
+// 	echo "Connection unsuccessful!";
+// }
 
 
 
