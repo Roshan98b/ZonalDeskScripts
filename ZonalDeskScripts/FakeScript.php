fc7306658ad3bd8ffe76fcbc0079c55c2d110b9c
Roshan Badrinath <roshan98b@gmail.com>
2018-11-11 17:47:00 +0530

    Cloud Test2

--- ZonalDeskScripts/FakeScript.php
+++ ZonalDeskScripts/FakeScript.php
@@ -3,17 +3,17 @@
 $dbName = "ZonalDesk";
 $mysqlUsername = "admin";
 $mysqlPassword = "LXCEACZADQWPUTXZ";
-$serverName = "sl-eu-gb-p00.dblayer.com";
+$serverName = "sl-eu-gb-p00.dblayer.com:15556";
 
 $connect = mysqli_connect($serverName,$mysqlUsername,$mysqlPassword,$dbName);
 
-/*if($connect) {
+if($connect) {
 	echo "Connection successful!";
 }
 else {
 	echo "Connection unsuccessful!";
 }
-*/
+
 
 
 ?>
