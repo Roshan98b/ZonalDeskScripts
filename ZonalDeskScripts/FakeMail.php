219fb2683120e4aae16650fa582d3a7704cbce76
Roshan Badrinath <roshan98b@gmail.com>
2018-11-12 13:00:37 +0530

    Mail2

--- ZonalDeskScripts/FakeMail.php
+++ ZonalDeskScripts/FakeMail.php
@@ -1,19 +1,24 @@
 <?php
-//$to       = 'tm747177@gmail.com';
+use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 
-$to = $_POST["email"];
-$OTP = $_POST["OTP"];
+$to = $_POST['email'];
+$OTP = $_POST['OTP'];
 
 $mail = new PHPMailer;
 
 $subject  = 'OTP for ZonalDesk Registration';
 $message  = "Your OTP for registration is:- $OTP <br>Please do not share it with anyone.<br>OTP will expire in 30mins.<br> <br>Please do not reply to this email as it is automatically generated.";
 
-$mail->From = "roshan98b@gmail.com";
-$mail->FromName = "Roshan Badrinath";
-
-//To address and name
+$mail->isSMTP(); 
+$mail->SMTPDebug = 0; 
+$mail->Host = "smtp.gmail.com";
+$mail->Username = "aaroncoc0010@gmail.com";                 
+$mail->Password = "biztime12";
+$mail->setFrom('aaroncoc0010@gmail.com');  
+$mail->Port =  587; 
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
 $mail->addAddress($to); //Recipient name is optional
 
 //Send HTML or Plain Text email
@@ -30,6 +35,4 @@ else
 {
     echo "Message has been sent successfully";
 }
-
-
 ?>
